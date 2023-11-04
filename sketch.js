dbd7975c1e2d68189183dcc9df82d7c2f23aed01
Ayushi Shah <as17726@nyu.edu>
2023-11-04 16:32:10 -0400

    html

--- sketch.js
+++ sketch.js
@@ -1,11 +1,21 @@
-function setup()
-{
+function setup() {
 	createCanvas(windowWidth, windowHeight);
+	background(250);
+	rectMode(CENTER);
 	
-}
-
-function draw()
-{
-	background(240);
-
-}
+	// Assuming PTN is a pattern drawing library object with PTN.checked() function
+	let checkedPattern = PTN.checked(20);
+	
+	// Draw a pattern using rectPattern() function
+	rectPattern(width / 2, height / 2, 400, 400, checkedPattern);
+  }
+  
+  function rectPattern(x, y, w, h, pattern) {
+	let patternSize = pattern.size;
+	for (let i = x - w / 2; i < x + w / 2; i += patternSize) {
+	  for (let j = y - h / 2; j < y + h / 2; j += patternSize) {
+		pattern.draw(i, j, patternSize, patternSize);
+	  }
+	}
+  }
+  
